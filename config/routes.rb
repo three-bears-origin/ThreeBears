0c752ca503e6921de06ba89411fe41a2d39434e8
CDBiddulph <caleb.d.biddulph@gmail.com>
2020-07-24 17:10:51 -0500

    Got PostgreSQL database working on localhost:3000
    Username is threebears, password is cirje

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'landing/index'
+  # get '/timeline', to: redirect('/events')
 
-  resources :events
+  resources :events, path: 'timeline'
 
   root 'landing#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

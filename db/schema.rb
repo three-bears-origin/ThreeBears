8f99da1836fbfca4483a9d723f6ac111140a574b
iris_li <irisli12345@gmail.com>
2020-07-26 01:14:16 -0400

    fixed db names

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_21_213316) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.date "date"

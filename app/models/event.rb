8f1813bb318031896eeb85b41134a8eaadf79272
CDBiddulph <caleb.d.biddulph@gmail.com>
2020-08-04 14:06:52 -0500

    One more merge error, trash wasn't working

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ApplicationRecord
-    has_many :contributions
-    #has_many :contributions, dependent: :destroy
+    has_many :contributions, dependent: :destroy
     validates :title, presence: true
     default_scope { order(date: :asc) } # makes it so Event.all will be in date order by default
 end
